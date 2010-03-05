117c4cf10e537ebf6fb8c79c9d1cc9d697868b39
Anders Janmyr <anders.janmyr@jayway.com>
2010-03-05 15:54:20 +0100

    First version of formtastic forms.
    DOES NOT WORK, YET

---
+++ lib/generators/formtastic_haml/scaffold/scaffold_generator.rb
@@ -0,0 +1,23 @@
+require 'generators/formtastic_haml/'
+require 'generators/erb/scaffold/scaffold_generator'
+
+module FormtasticHaml
+  module Generators
+    class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
+      extend TemplatePath
+
+      def copy_layout_file
+        return unless options[:layout]
+        template "layout.haml.erb",
+                 File.join("app/views/layouts", controller_class_path, "#{controller_file_name}.html.haml")
+      end
+
+      protected
+
+        def copy_view(view)
+          template "#{view}.haml.erb", File.join("app/views", controller_file_path, "#{view}.html.haml")
+        end
+
+    end
+  end
+end
