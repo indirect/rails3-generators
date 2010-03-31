b833acbc69c408e089d60a0cbd8ad401b3cc84f3
guillaume belleguic <guillaume@MacBook-Pro-de-guillaume-belleguic.local>
2010-03-31 19:53:38 +0200

    remove timestamps!

--- lib/generators/mongoid/model/templates/model.rb
+++ lib/generators/mongoid/model/templates/model.rb
@@ -2,7 +2,6 @@ class <%= class_name %><%= "< #{options[:parent].classify}" if options[:parent] 
 <% unless options[:parent] -%>
   include Mongoid::Document 
   include Mongoid::Timestamps
-  timestamps!
 <% end -%>
 <% attributes.each do |attribute| -%>
   field :<%= attribute.name %>, <%= attribute.type_class %>
