584199d5f51a71ef04f2754f5d6a95a4c96dd34d
Peter Haza <peter.haza@gmail.com>
2010-04-09 20:56:27 +0200

    Fix typos and add hook for fixture replacement

--- lib/generators/shoulda/controller/controller_generator.rb
+++ lib/generators/shoulda/controller/controller_generator.rb
@@ -1,4 +1,4 @@
-require 'generators/haml'
+require 'generators/shoulda'
 require 'rails/generators/named_base'
 
 module Shoulda
@@ -7,11 +7,15 @@ module Shoulda
       
       argument :actions, :type => :array, :default => [], :banner => "action action"
       class_option :dir, :type => :string, :default => "test/functional", :desc => "The directory where the controller tests should go"
+      
+      check_class_collision :suffix => 'ControllerTest'
 
       def create_controller_file
-        template 'controller.rb', File.join(options[:dir], "#{plural_name}_controller_test.rb")
+        template 'controller.rb', File.join(options[:dir], "#{file_name}_controller_test.rb")
       end
-
+      
+      hook_for :fixture_replacement
+      
     end
   end
 end
