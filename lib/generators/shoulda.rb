f75d2f0ad8f66aa8ac76fef1f2dd070e2bcdb98c
Peter Haza <peter.haza@gmail.com>
2010-04-09 21:24:46 +0200

    Add some missing helpers from nifty scaffold

--- lib/generators/shoulda.rb
+++ lib/generators/shoulda.rb
@@ -6,6 +6,37 @@ module Shoulda
       def self.source_root
         @_shoulda_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'shoulda', generator_name, 'templates'))
       end
+      
+      
+      # nicked from nifty_scaffold
+      
+      def action?(name)
+         controller_actions.include? name.to_s
+       end
+      
+      def items_path(suffix = 'path')
+        if action? :index
+          "#{plural_name}_#{suffix}"
+        else
+          "root_#{suffix}"
+        end
+      end
+
+      def item_path(suffix = 'path')
+        if action? :show
+          "@#{singular_name}"
+        else
+          items_path(suffix)
+        end
+      end
+
+      def item_path_for_test(suffix = 'path')
+        if action? :show
+          "#{singular_name}_#{suffix}(assigns(:#{singular_name}))"
+        else
+          items_path(suffix)
+        end
+      end
     end
   end
 end
