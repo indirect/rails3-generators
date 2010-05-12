218b991c44f5e74925907d99c391d44925db6207
Anders Janmyr <anders.janmyr@jayway.com>
2010-05-12 08:21:29 +0200

    Updated formtastic generators to newer beta of rails

--- lib/generators/formtastic_erb/scaffold/scaffold_generator.rb
+++ lib/generators/formtastic_erb/scaffold/scaffold_generator.rb
@@ -1,5 +1,5 @@
 require 'generators/formtastic_erb'
-require 'generators/erb/scaffold/scaffold_generator'
+require 'rails/generators/erb/scaffold/scaffold_generator'
 
 module FormtasticErb
   module Generators
@@ -8,20 +8,18 @@ module FormtasticErb
 
       def copy_layout_file
         return unless options[:layout]
-        template "layout.html.erb",
+        template "layout.haml.erb",
                  File.join("app/views/layouts", controller_class_path, "#{controller_file_name}.html.erb")
       end
+      
+      def copy_view_files
+        views = available_views
+        views.delete("index") if options[:singleton]
 
-      protected
-
-      def copy_view(view)
-        if view =~ /^_.*/
+        views.each do |view|
           template "#{view}.html.erb", File.join("app/views", controller_file_path, "#{view}.html.erb")
-        else
-          super view
         end
       end
-
     end
   end
 end
