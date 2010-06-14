e550d7f39fb95f4f24ef4de9585f6dad61e16549
Louis T <negonicrac@gmail.com>
2010-06-14 02:54:46 -0400

    renamed haml:install test to follow standards

--- test/generators/haml/install_generator_test.rb
+++ test/generators/haml/install_generator_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'generators/haml/install/install_generator'
 
 class Haml::Generators::InstallGeneratorTest < Rails::Generators::TestCase
-  destination File.join(Rails.root, "tmp")
+  destination File.join(Rails.root)
   tests Haml::Generators::InstallGenerator
   arguments []
 
