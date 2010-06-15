2898a871aaea64e1e4e4693c2d7cb8b6dc311cf3
Michael Harrison <mh@michaelharrison.ws>
2010-06-15 15:14:55 -0400

    First commit of controller generator code for mustache
    Copied Paul Barry's HAML files and modified the controller generator
      files; generator only creates blank files for now.
    
    Signed-off-by: Louis T <negonicrac@gmail.com>

---
+++ lib/generators/mustache_generator/controller/controller_generator.rb
@@ -0,0 +1,36 @@
+require 'generators/mustache_generator'
+require 'rails/generators/named_base'
+
+module MustacheGenerator
+  module Generators
+    class ControllerGenerator < Rails::Generators::NamedBase
+      extend TemplatePath
+      
+      argument :actions, :type => :array, :default => [], :banner => "action action"
+
+      def create_view_files
+        model_path                  = File.join(class_path, file_name)
+        
+        base_mustache_view_path     = File.join("app/views", model_path)
+        empty_directory base_mustache_view_path
+        
+        base_mustache_template_path = File.join("app/templates", model_path)
+        empty_directory base_mustache_template_path
+        
+        actions.each do |action|
+          @action  = action
+          @mustache_view_path       = File.join(base_mustache_view_path,
+                                                "#{action}.rb")
+          @mustache_template_path   = File.join(base_mustache_template_path,
+                                                "#{action}.html.mustache")
+                                              
+          # TODO Check for action in list
+          create_file @mustache_view_path
+          create_file @mustache_template_path
+          # template 'view.rb',            @mustache_view_path                               
+          # template 'view.html.mustache', @mustache_template_path
+        end
+      end
+    end
+  end
+end
