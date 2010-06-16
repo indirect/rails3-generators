8269818ec6250ce013bb5772dab439f80d41bb14
Louis T <negonicrac@gmail.com>
2010-06-16 02:35:47 -0400

    remove unneeded form builder scaffolds

--- lib/rails3-generators.rb
+++ lib/rails3-generators.rb
@@ -1,2 +1,57 @@
+require 'rails/generators'
+
 module Rails3Generators
 end
+
+Rails::Generators.hidden_namespaces << "rails"
+
+%w(active_record datamapper mongomapper).each do |orm|
+  Rails::Generators.hidden_namespaces <<
+  [
+    "#{orm}:migration",
+    "#{orm}:model",
+    "#{orm}:observer",
+    "#{orm}:session_migration",
+  ]
+end
+
+%w(test_unit rspec shoulda).each do |test|
+  Rails::Generators.hidden_namespaces <<
+  [
+    "#{test}:controller",
+    "#{test}:helper",
+    "#{test}:integration",
+    "#{test}:mailer",
+    "#{test}:model",
+    "#{test}:observer",
+    "#{test}:scaffold",
+    "#{test}:view",
+    "#{test}:performance",
+    "#{test}:plugin",
+  ]
+end
+
+%w(factory_girl machinist).each do |fixture_replacement|
+  Rails::Generators.hidden_namespaces <<
+  [
+    "#{fixture_replacement}:model",
+  ]
+end
+
+%w(erb erubis haml).each do |template|
+  Rails::Generators.hidden_namespaces <<
+  [
+    "#{template}:controller",
+    "#{template}:scaffold",
+    "#{template}:mailer"
+  ]
+end
+
+%w(formtastic simple_form).each do |form_builder|
+  Rails::Generators.hidden_namespaces <<
+  [
+    "#{form_builder}:scaffold"
+  ]
+end
+
+Rails::Generators.hidden_namespaces.flatten!
