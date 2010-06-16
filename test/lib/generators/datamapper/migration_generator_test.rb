13427d65049a004ae43b63c896a2e8731816a292
Louis T <negonicrac@gmail.com>
2010-06-16 00:56:25 -0400

    updated Mongomapper orm. lacking tests.

--- test/lib/generators/datamapper/migration_generator_test.rb
+++ test/lib/generators/datamapper/migration_generator_test.rb
@@ -7,7 +7,7 @@ class Datamapper::Generators::MigrationGeneratorTest < Rails::Generators::TestCa
   setup :prepare_destination
   setup :copy_routes
 
-  test "invoke migration" do
-    flunk "I don't use datamapper. If you use it please add tests."
+  test "invoke" do
+    flunk "I don't use Datamapper. If you use it please add tests."
   end
 end
--- test/lib/generators/datamapper/model_generator_test.rb
+++ test/lib/generators/datamapper/model_generator_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class Datamapper::Generators::ModelGeneratorTest < Rails::Generators::TestCase
   destination File.join(Rails.root)
-  tests Datamapper::Generators::MigrationGenerator
+  tests Datamapper::Generators::ModelGenerator
 
   setup :prepare_destination
   setup :copy_routes
 
-  test "invoke migration" do
-    flunk "I don't use datamapper. If you use it please add tests."
+  test "invoke" do
+    flunk "I don't use Datamapper. If you use it please add tests."
   end
 end
