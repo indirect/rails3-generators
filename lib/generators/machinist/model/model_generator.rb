0dc2d5a1f71471302f1d66af52d8566802d05f5d
Louis T <negonicrac@gmail.com>
2010-07-11 20:57:51 -0400

    Version bump to 0.10.3

--- lib/generators/machinist/model/model_generator.rb
+++ lib/generators/machinist/model/model_generator.rb
@@ -5,7 +5,7 @@ module Machinist
     class ModelGenerator < Base
       argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
       class_option :dir, :type => :string, :default => "test/blueprints", :desc => "The directory where the blueprints should go"
-      class_option :extension, :type => :string, :default => "rb", :desc => "file extenstion name"
+      class_option :extension, :type => :string, :default => "rb", :desc => "file extension name"
       
       def create_machinist_initializer
         return if File.exists?(File.expand_path("config/initializers/machinist.rb", self.destination_root))
@@ -13,7 +13,7 @@ module Machinist
       end
       
       def create_blueprint_file
-        template 'blueprint.rb', File.join(options[:dir], "#{table_name}.#{options[:extenstion].to_s}")
+        template 'blueprint.rb', File.join(options[:dir], "#{table_name}.#{options[:extension].to_s}")
       end
     end
   end
