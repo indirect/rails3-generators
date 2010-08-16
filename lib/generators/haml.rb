72c4460bf57bfe593d50a5f76c5161b039d6a433
Andre Arko <andre@arko.net>
2010-08-16 12:28:09 -0700

    Haml generators moved to haml-rails gem

--- lib/generators/haml.rb
+++
@@ -1,9 +0,0 @@
-module Haml
-  module Generators
-    module TemplatePath
-      def source_root
-        @_haml_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'haml', generator_name, 'templates'))
-      end
-    end
-  end
-end
