b6caeff85f50a3af05cfd8fa4a8c1434012f063f
Kevin Faustino <kevin.faustino@gmail.com>
2011-01-23 14:19:23 -0500

    Update FactoryGirl generator template to use syntax from version 2.0.0

--- lib/generators/factory_girl/model/templates/fixtures.rb
+++ lib/generators/factory_girl/model/templates/fixtures.rb
@@ -1,7 +1,9 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
-Factory.define :<%= singular_name %> do |f|
-<% for attribute in attributes -%>
-  f.<%= attribute.name %> <%= attribute.default.inspect %>
-<% end -%>
+FactoryGirl.define do
+  factory :<%= singular_name %> do
+  <% for attribute in attributes -%>
+    <%= attribute.name %> <%= attribute.default.inspect %>
+  <% end -%>
+  end
 end
