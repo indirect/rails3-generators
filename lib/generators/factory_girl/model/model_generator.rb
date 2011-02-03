094caf6edc71b156c5ac66d047406c56c0ab07d1
Louis T <negonicrac@gmail.com>
2011-02-02 21:00:03 -0500

    Allow use of both factory_girl 1 and 2.

--- lib/generators/factory_girl/model/model_generator.rb
+++ lib/generators/factory_girl/model/model_generator.rb
@@ -7,7 +7,18 @@ module FactoryGirl
       class_option :dir, :type => :string, :default => "test/factories", :desc => "The directory where the factories should go"
       
       def create_fixture_file
-        template 'fixtures.rb', File.join(options[:dir], "#{table_name}.rb")
+        version = nil
+        begin
+          require 'factory_girl/version'
+          version = FactoryGirl::VERSION.to_i
+        rescue LoadError
+          require 'factory_girl'
+          version = Factory::VERSION.to_i
+        rescue Object => e
+          raise "Please install Factory_girl or add it to your Gemfile"
+        end
+        
+        template "fixtures.#{version}.rb", File.join(options[:dir], "#{table_name}.rb")
       end
     end
   end
--- rails3-generators.gemspec
+++ rails3-generators.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency "test-unit"
   s.add_development_dependency "haml-rails"
   s.add_development_dependency "rails", ">= 3.0.0"
+  s.add_development_dependency "factory_girl"
   
   s.files        = `git ls-files`.split("\n")
   s.executables  = `git ls-files`.split("\n").select{|f| f =~ /^bin/}
