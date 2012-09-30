b4eaf0f1fd7c138b17de6db98b356bec02a46819
Louis T <louis@negonicrac.com>
2012-09-29 20:01:05 -0400

    update Gemfile.lock

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,10 +18,6 @@ module Rails
 end
 Rails.application.config.root = Rails.root
 
-# Call configure to load the settings from
-# Rails.application.config.generators to Rails::Generators
-Rails::Generators.configure!
-
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 def copy_routes
